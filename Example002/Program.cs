a83cfc770fe1edca488a4f85a84465cebf314c91
Kate Belozertseva <katebelozertseva@gmail.com>
2022-08-29 23:55:11 +0300

    SecondFolder + gitignore

---
+++ Example002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name, please");
+string username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
+//не забывать ; и важна каждая заглавная и маленькая буква!
+
+Console.Write("Enter your name, please");
+string usernameA = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(usernameA);
+//WriteLine - на новой строчке; Write - на одной
+
