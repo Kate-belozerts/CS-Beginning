607216be0090c9e506a7dfd59323eb6b158a9bf0
Kate Belozertseva <katebelozertseva@gmail.com>
2022-08-30 19:40:46 +0300

    Will Smith))

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name, pls ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "will smith") //ToLower - переводит всё на маленькие буквы. => по умолчанию тоже нужно ставить всё маленькими!
+{
+    Console.WriteLine("Wow, that's Will !!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
