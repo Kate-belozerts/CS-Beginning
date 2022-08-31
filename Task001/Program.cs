2618660b6b31e812cc9fd5f9e6075e1b134fe6b7
Kate Belozertseva <katebelozertseva@gmail.com>
2022-08-31 20:18:59 +0300

    Find square

---
+++ Task001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Enter integer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square}");
+Console.WriteLine("Квадрат числа" + num + "=" + square);
+// -2 147 648 млн до 4 147 483 647
+
